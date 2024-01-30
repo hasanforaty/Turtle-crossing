317b43d9301c17c9f39eac06db76e952213f6d63
hasan foraty <hasanforaty19@gmail.com>
2024-01-30 10:48:42 +0330

    create main

---
+++ main.py
@@ -0,0 +1,16 @@
+from turtle import Turtle, Screen
+from time import sleep
+
+screen = Screen()
+screen.setup(
+    width=600,
+    height=600,
+)
+screen.tracer(0)
+
+game_is_on = True
+while game_is_on:
+    sleep(0.1)
+    screen.update()
+
+screen.exitonclick()
