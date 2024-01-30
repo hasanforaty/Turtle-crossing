3b1a598dbe95296dd8f1f25596c907a9f1442695
hasan foraty <hasanforaty19@gmail.com>
2024-01-30 11:00:14 +0330

    create turtle and move

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 from turtle import Turtle, Screen
 from time import sleep
+from player import Player
 
 screen = Screen()
+player = Player()
 screen.setup(
     width=600,
     height=600,
 )
 screen.tracer(0)
+screen.bgcolor("white")
+
+screen.listen()
+screen.onkeypress(
+    player.move,
+    'w'
+)
+
 
 game_is_on = True
 while game_is_on:
--- player.py
+++ player.py
@@ -1,7 +1,20 @@
+from turtle import Turtle
+
 STARTING_POSITION = (0, -280)
 MOVE_DISTANCE = 10
 FINISH_LINE_Y = 280
 
 
-class Player:
-    pass
+class Player(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.hideturtle()
+        self.penup()
+        self.goto(STARTING_POSITION)
+        self.shape("turtle")
+        self.setheading(90)
+        self.color("black")
+        self.showturtle()
+
+    def move(self):
+        self.forward(MOVE_DISTANCE)
