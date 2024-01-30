8b05d873832964d9bc617abec490ae2485cb978e
hasan foraty <hasanforaty19@gmail.com>
2024-01-30 11:56:45 +0330

    add scoreboard and Level to game finished game

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from turtle import Turtle, Screen
 from time import sleep
 from player import Player, FINISH_LINE_Y
+from scoreboard import Scoreboard
 from car_manager import CarManager
 import random
 
 screen = Screen()
 player = Player()
+scoreboard = Scoreboard()
 car_manager = CarManager()
 screen.setup(
     width=600,
@@ -32,13 +34,17 @@ while game_is_on:
         car_manager.create_random_car()
 
     # move cars
-    car_manager.move_cars(level=1)
+    car_manager.move_cars(level=scoreboard.current_level)
 
     # check if player reached end point
     if player.ycor() > FINISH_LINE_Y:
+        scoreboard.increase_level()
+        scoreboard.print_level()
         player.reset()
     # check for coalition with cars
     if car_manager.has_collided(player):
+        scoreboard.game_over()
+        screen.update()
         game_is_on = False
 
 screen.exitonclick()
--- scoreboard.py
+++ scoreboard.py
@@ -1,5 +1,25 @@
+from turtle import Turtle
+
 FONT = ("Courier", 24, "normal")
 
 
-class Scoreboard:
-    pass
+class Scoreboard(Turtle):
+    def print_level(self):
+        self.clear()
+        self.goto(x=-200, y=250)
+
+        self.write(f"Level: {self.current_level}", align="center", font=FONT)
+
+    def __init__(self):
+        super().__init__()
+        self.penup()
+        self.hideturtle()
+        self.current_level = 1
+        self.print_level()
+
+    def increase_level(self):
+        self.current_level += 1
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write("GAME OVER", align="center", font=FONT)
